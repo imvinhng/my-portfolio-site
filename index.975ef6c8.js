1573c286fa761380b5d023d98effb914f4ee6060
Young Programmer <63667978+imvinhng@users.noreply.github.com>
2023-05-30 22:26:04 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28622,8 +28622,8 @@ var _propTypesDefault = parcelHelpers.interopDefault(_propTypes);
  *
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
- */ var _tempPortfolioPicEditedJpg = require("../images/temp-portfolio-pic-edited.jpg");
-var _tempPortfolioPicEditedJpgDefault = parcelHelpers.interopDefault(_tempPortfolioPicEditedJpg);
+ */ var _tempPortfolioPicJpg = require("../images/temp-portfolio-pic.jpg");
+var _tempPortfolioPicJpgDefault = parcelHelpers.interopDefault(_tempPortfolioPicJpg);
 const imageAltText = "Adult male in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 const Home = ({ name , title  })=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("section", {
@@ -28632,7 +28632,7 @@ const Home = ({ name , title  })=>{
         children: [
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("img", {
                 className: "background",
-                src: (0, _tempPortfolioPicEditedJpgDefault.default),
+                src: (0, _tempPortfolioPicJpgDefault.default),
                 alt: ""
             }, void 0, false, {
                 fileName: "src/Components/Home.jsx",
@@ -28715,11 +28715,11 @@ $RefreshReg$(_c, "Home");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","../images/temp-portfolio-pic-edited.jpg":"9NXTk","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"b5nCi":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","../images/temp-portfolio-pic.jpg":"cvxJn"}],"b5nCi":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "down-arrow.1037091f.svg" + "?" + Date.now();
 
-},{"./helpers/bundle-url":"lgJ39"}],"9NXTk":[function(require,module,exports) {
-module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "temp-portfolio-pic-edited.6bf9e0fc.jpg" + "?" + Date.now();
+},{"./helpers/bundle-url":"lgJ39"}],"cvxJn":[function(require,module,exports) {
+module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "temp-portfolio-pic.a5c01462.jpg" + "?" + Date.now();
 
 },{"./helpers/bundle-url":"lgJ39"}],"lCaEt":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$ed0d = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");

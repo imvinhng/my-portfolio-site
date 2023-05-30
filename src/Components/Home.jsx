0b5b7d03666329123d8f1a2e7b624820703ca47b
Young Programmer <63667978+imvinhng@users.noreply.github.com>
2023-05-30 22:26:19 +0000

    change the picture

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/temp-portfolio-pic-edited.jpg";
+import image from "../images/temp-portfolio-pic.jpg";
 
 const imageAltText = "Adult male in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 

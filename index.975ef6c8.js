27936e752d61e3d1edcb47b65cee4d14bbbe6a88
Young Programmer <63667978+imvinhng@users.noreply.github.com>
2023-05-31 01:08:05 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28715,7 +28715,7 @@ $RefreshReg$(_c, "Home");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","../images/temp-portfolio-pic.jpg":"cvxJn"}],"b5nCi":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/down-arrow.svg":"b5nCi","prop-types":"7wKI2","../images/temp-portfolio-pic.jpg":"cvxJn","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"b5nCi":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "down-arrow.1037091f.svg" + "?" + Date.now();
 
 },{"./helpers/bundle-url":"lgJ39"}],"cvxJn":[function(require,module,exports) {
@@ -28766,7 +28766,7 @@ const imageAltText = "desktop with books and laptop";
     {
         title: "Space Invader X",
         description: "A recreation of the classic Space Invader game for the Global Game Jam of 2020. Built by Unity game engine, by the our team the Space Jammers. ",
-        url: "https://github.com/imvinhng/money-plant-ttseattle2023"
+        url: "https://github.com/imvinhng/GlobalGameJam2020"
     },
     {
         title: "The Zombie Killer",
